e504403e5639c3fb17eaf0a5f2d6aca2ec6d61c0
José Valim <jose.valim@gmail.com>
2009-05-29 12:25:25 +0200

    More documentation to have_action and updating CHANGELOG [#81 status:resolved].

--- remarkable_rails/lib/remarkable_rails/action_controller/matchers/have_actions_matcher.rb
+++ remarkable_rails/lib/remarkable_rails/action_controller/matchers/have_actions_matcher.rb
@@ -3,31 +3,31 @@ module Remarkable
     module Matchers
       # Do not inherit from ActionController::Base since it don't need all macro stubs behavior.
       class HaveActionsMatcher < Remarkable::Base #:nodoc:
-        arguments :collection => :params, :as => :param
+        arguments :collection => :actions, :as => :action
 
         collection_assertions :has_action?
 
         protected
 
           def has_action?
-            [@param].flatten.each { |param| return false unless @subject.class.instance_methods.include?(param.to_s) }
-            true
+            subject_class.action_methods.include?(@action.to_s)
           end
-
       end
 
-      # Checks if the controller respond to the given actions.
+      # Checks if the controller respond to the given actions. You might want 
+      # use this matcher in its negative form though, to ensure that you are
+      # not exposing a method to a resource.
       #
       # == Examples
       #
-      #   should_have_actions [:new, :create]
-      #   should_have_action :show
+      #   should_have_action :destroy
+      #   should_not_have_actions :new, :create
       #
-      #   it { should have_actions([:new, :create]) }
-      #   it { should have_action(:show) }
+      #   it { should have_action(:destroy) }
+      #   it { should_not have_actions(:new, :create) }
       #
-      def have_actions(*params)
-        HaveActionsMatcher.new(*params).spec(self)
+      def have_actions(*args, &block)
+        HaveActionsMatcher.new(*args, &block).spec(self)
       end
       alias :have_action :have_actions
 
